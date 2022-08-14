d33b385beb7c0c266d5835bd605a5c8eb5c28202
AFENIKHENA FAVOUR <63492224+codertjay@users.noreply.github.com>
2022-08-14 03:23:35 -0700

    The validate email required
    I fixed the email required from 
     this
    `json:"email" validate:"email,required"`
    to 
    `json:"email" validate:"required"`
    
    it was actually giving an error

--- models/userModel.go
+++ models/userModel.go
@@ -10,7 +10,7 @@ type User struct{
 	First_name		*string					`json:"first_name" validate:"required,min=2,max=100"`
 	Last_name		*string					`json:"last_name" validate:"required,min=2,max=100"`
 	Password		*string					`json:"Password" validate:"required,min=6"`
-	Email			*string					`json:"email" validate:"email,required"`
+	Email			*string					`json:"email" validate:"required"`
 	Phone			*string					`json:"phone" validate:"required"`
 	Token			*string					`json:"token"`
 	User_type		*string					`json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
